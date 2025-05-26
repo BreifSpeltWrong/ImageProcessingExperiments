39ac42c577ba5247f9183faec8903ea73558df78
Zfish8789 <90352232+Zfish8789@users.noreply.github.com>
2025-05-26 21:37:29 +0100

    A start on meanfilter

--- src/Filters/Meanfilter.java
+++ src/Filters/Meanfilter.java
@@ -1,15 +1,26 @@
 package Filters;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
 
 public class Meanfilter extends Filter {
 
-    public Meanfilter(){
+    private int kernalsize;
 
+    public Meanfilter(int kernalsize){
+        this.kernalsize = kernalsize;
     }
 
     @Override
     public BufferedImage filterImage(BufferedImage inputImage) {
+        Raster rawImage = inputImage.getData();
+        for(int hi = 0; hi <= rawImage.getHeight(); hi++){
+            for(int wi = 0; wi <= rawImage.getWidth(); wi++){
+                int[] kernal = rawImage.getPixels(wi,hi,kernalsize,kernalsize,new int[] {});
+            }
+        }
+
+
         return null;
     }
 
