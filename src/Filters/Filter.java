a2ebb7395e9c0f7b75cd164ad4608b7fa81da5c3
BreifSpeltWrong <furrypuppy.al@gmail.com>
2025-05-26 20:33:54 +0100

    Creation of the Filter abstract class. This is defined in such a manner that it can be extended and then quickly added as an extra option.

---
+++ src/Filters/Filter.java
@@ -0,0 +1,30 @@
+package Filters;
+
+import java.awt.image.BufferedImage;
+
+public abstract class Filter {
+    /**
+     * Filters the input image to return an output.
+     * @param inputImage The image to be filtered. This image should be preserved and not edited.
+     * @return The edited image
+     */
+    public abstract BufferedImage filterImage(BufferedImage inputImage);
+
+    /**
+     * Returns the name of the filter in a human-readable format for display
+     * @return The name
+     */
+    public abstract String getName();
+
+    /**
+     * A short description of the filter, for a summary explanation of the filters effects.
+     * @return The description
+     */
+    public abstract String getDesc();
+
+    /**
+     * The names of any bonus arguments required - This is required to be in the same order as is needed for input to the filter.
+     * @return An array of argument names
+     */
+    public abstract String[] getBonusArgs();
+}
