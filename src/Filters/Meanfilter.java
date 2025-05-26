66738b617b869f72857622ac06df8b9218276775
Zfish8789 <90352232+Zfish8789@users.noreply.github.com>
2025-05-27 00:15:59 +0100

    Fixed bugs with Meanfilter (all typos) and added comments

--- src/Filters/Meanfilter.java
+++ src/Filters/Meanfilter.java
@@ -13,33 +13,36 @@ public class Meanfilter extends Filter {
     @Override
     public BufferedImage filterImage(BufferedImage inputImage) {
         BufferedImage newImage = new BufferedImage(inputImage.getWidth(),inputImage.getHeight(),BufferedImage.TYPE_INT_RGB);
+
+        //Goes through Pixel by pixel
         for(int x = 0; x < newImage.getWidth(); x++){
             for(int y = 0; y < newImage.getHeight(); y++){
+
                 int[][] kernal= new int[kernalsize][kernalsize];
                 int r = 0, g =0, b = 0;
 
-                for(int q = -1; q <= 1; q++){
-                    for(int v = -1; v <= 1; v++){
+                //Searches a <kernalsize> Grid arround the pixel
+                int searchSize = kernalsize/2;
+                for(int q = -searchSize; q <= searchSize; q++){
+                    for(int v = -searchSize; v <= searchSize; v++){
+
+                        //Stores the RGB data of the pixel or 0 if it's not there
                         try{
                             kernal[1+q][1+v] = inputImage.getRGB(x+q,y+v);
                         }catch (ArrayIndexOutOfBoundsException aE){
                             kernal[1+q][1+v] = 0;
                         }
 
-                        System.out.print(kernal[1+q][1+v] + "\t");
-
-                        r = (kernal[1+q][1+v] >> 16) & 0xFF;
-                        g = (kernal[1+q][1+v] >> 8) & 0xFF;
-                        b =  kernal[1+q][1+v] & 0xFF;
-
-                        System.out.println(r + "\t" + g + "\t" + b);
+                        //Sums the R G and B values
+                        r += (kernal[1+q][1+v] >> 16) & 0xFF;
+                        g += (kernal[1+q][1+v] >> 8) & 0xFF;
+                        b +=  kernal[1+q][1+v] & 0xFF;
                     }
                 }
 
+                // divided the R G and B values by the gridsize and reformats it
                 int kerSqr = kernalsize*kernalsize;
-                int newRGB = ((r/kerSqr) << 16) + ((g/kerSqr) << 16) + ((b/kerSqr));
-
-                System.out.println(newRGB);
+                int newRGB = ((r/kerSqr) << 16) + ((g/kerSqr) << 8) + ((b/kerSqr));
 
                 newImage.setRGB(x,y, newRGB);
             }
